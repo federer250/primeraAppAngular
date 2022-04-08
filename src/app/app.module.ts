bcdf05a8495796e21ccb174e462e3c3c0d092fb5
fede <fede@desktop>
2022-04-08 10:10:58 -0300

    primer commit en angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ButtonComponent } from './component/button/button.component';
+import { TasksComponent } from './component/tasks/tasks.component';
+import { TasksItemComponent } from './component/tasks-item/tasks-item.component';
+import { AddTaskComponent } from './component/add-task/add-task.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './component/about/about.component';
+import { FooterComponent } from './component/footer/footer.component';
+
+
+const appRoutes: Routes = [
+  {path: '', component: TasksComponent},
+  {path: 'about', component: AboutComponent}
+
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TasksItemComponent,
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot (appRoutes ,{enableTracing: true})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,29 @@
+import { Task } from "./task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "terminar primer modulo de angular",
+        day: 'Agosto 5 a las 12:00',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "hacer las compras para la casa",
+        day: 'Agosto 5 a las 17:00',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "investigar sobre bootstrap",
+        day: 'Agosto 5 a las 19:00',
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "leer mi libro favorito",
+        day: 'Agosto 5 a las 21:00',
+        reminder: false
+    },
+ 
+]
